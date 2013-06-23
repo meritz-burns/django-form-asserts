e4861b8bc6060d6efdda4692d51920937fca099c
Rebecca Meritz and Mike Burns <rjmeritz+burns@gmail.com>
2013-06-23 08:38:35 +0200

    wip

--- django_form_asserts/test/test_fields.py
+++ django_form_asserts/test/test_fields.py
@@ -2,25 +2,45 @@ from django import forms
 import unittest
 from unittest import TestCase
 
+from django_form_asserts import FormTestCase
+
 
 class TestFields(TestCase):
     def build_form(self, field_name, field):
         form = forms.Form()
-        form.fields[field_name]=field
+        form.fields[field_name] = field
         return form
 
-    def runAssertion(self, assertion, form, field_name, **kwargs):
+    def build_form_class(self, form):
+        klass = type("tmp_form_class", (forms.Form,), {})
+        klass.fields = form.fields
+        return klass
 
-    
+    def build_test_case(self, form, valid_attrs):
+        form_class = self.build_form_class(form)
+        test_case = FormTestCase('runTest')
+        test_case.form_class = form_class
+        test_case.valid_attrs = valid_attrs
+        return test_case
+
     def test_boolean_field(self):
         field = forms.BooleanField()
+        valid_attrs = {"is_cute": True}
         form = self.build_form("is_cute", field)
-        self.runAssertion(self.assertBooleanField, form, 'is_cute')
+        form.is_cute = True
+        test_case = self.build_test_case(form, valid_attrs)
+        test_case.assertBooleanField('is_cute')
+        form.is_cute = False
+        test_case = self.build_test_case(form, valid_attrs)
+        test_case.assertBooleanField('is_cute')
+        form.is_cute = None
+        test_case = self.build_test_case(form, valid_attrs)
+        test_case.assertBooleanField('is_cute')
 
-    def test_boolean_field_required(self):
-        field = forms.BooleanField(required=True)
-        form = self.build_form("is_cute", field)
-        self.runAssertion(self.assertBooleanField, form, 'is_cute', required=True)
+    #def test_boolean_field_required(self):
+    #    field = forms.BooleanField(required=True)
+    #    form = self.build_form("is_cute", field)
+    #    self.runAssertion(self.assertBooleanField, form, 'is_cute', required=True)
 
 if __name__ == '__main__':
     unittest.main()
